47a0d0249ff71b93950b658e9e904f57e2520a30
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-05-15 21:09:39 +0200

    Projektdateien hinzufügen.

--- ArtikelUebung/ArtikelUebung/Program.cs
+++ ArtikelUebung/ArtikelUebung/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Artikel1 artikel = new Artikel1(119.99m, "XBox Spiel Monster Hunter Wild", 36789);
+=======
+            Artikel1 artikel = new Artikel1(119.99m, "(XBox Spiel)Monster Hunter Wilds", 36789);
+>>>>>>> Projektdateien hinzufügen.
             Console.WriteLine(artikel.GetDetails());
         }
     }
