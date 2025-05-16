67287b6466194b334974c05764f1ffe23134cc6b
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-05-16 17:14:44 +0200

    kleine verbesserung sprich kommentar rausgenommen

--- ArtikelUebung/ArtikelUebung/Program.cs
+++ ArtikelUebung/ArtikelUebung/Program.cs
@@ -4,11 +4,7 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Artikel1 artikel = new Artikel1(119.99m, "XBox Spiel Monster Hunter Wild", 36789);
-=======
             Artikel1 artikel = new Artikel1(119.99m, "(XBox Spiel)Monster Hunter Wilds", 36789);
->>>>>>> Projektdateien hinzufügen.
             Console.WriteLine(artikel.GetDetails());
         }
     }
