8ea97a14f82501c18526d4e085d8a85cdf54aaa1
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-05-18 00:10:42 +0200

    Einfürhung C#

---
+++ EinfuehrungCSharp/EinfuehrungCSharp/Auto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Auto
+{
+	public class Auto()
+	{
+		public string model;
+		public int baujahr;
+		public string farbe;
+		public int geschwindigkeit;
+
+		public Auto(string m, int bauj, string f)
+        {
+            model = m;
+            baujahr = bauj;
+            farbe = f;
+        }
+
+		public void autoAnzeigen()
+        {
+            Console.WriteLine("Auto-Model:", + model);
+			Console.WriteLine("Baujahr:", + baujahr);
+			Console.WriteLine("Farbe:", + farbe);
+			Console.WriteLine("================================================");
+        }
+
+		public void autoFahren()
+        {
+            Console.WriteLine("Das Auto " + model + " fährt mit " + geschwindigkeit + "km/H");
+        }
+
+		public void autoBeschleunigen()
+		{
+			geschwindigkeit += 10
+		}
+
+		public void autoBremsen()
+        {
+            if (geschwindigkeit > 0)
+            {
+                geschwindigkeit -= 10;
+            }
+            else
+            {
+                Console.WriteLine("Das Auto ist bereits gestoppt.");
+            }
+        }
+	}
+}
--- EinfuehrungCSharp/EinfuehrungCSharp/Program.cs
+++ EinfuehrungCSharp/EinfuehrungCSharp/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace EinfuehrungCSharp
+﻿using System;
+
+namespace EinfuehrungCSharp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Erstelle eines Objekts der Klasse Auto
+            Auto a1 = new Auto("BMW X5", 2020, "Anthrazit");
+
+            a1.autoanzeigen();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobremsen();
+            a1.autofahren();
+            a1.autobremsen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
+            a1.autobeschleunigen();
+            a1.autofahren();
         }
     }
 }
