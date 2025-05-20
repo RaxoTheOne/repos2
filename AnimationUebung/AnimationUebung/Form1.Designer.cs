7542f233cd9f52e04e855caf96f1038ffcdd56fa
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-05-20 22:55:14 +0200

    Neue Ampel und Fahrzeug hinzugefügt, UI und Logik erweitert
    Es wurden neue Steuerelemente (`PictureBox` und Timer) für eine
    zweite Ampel und ein zweites Fahrzeug hinzugefügt. Die Logik
    für die Steuerung und Animation der neuen Elemente wurde in
    separaten Methoden implementiert. Bestehende Steuerelemente
    wurden in Position und Größe angepasst, um Platz zu schaffen.
    
    Zusätzlich wurde die Rückwärtsbewegung für beide Fahrzeuge
    integriert, und der Geschwindigkeitsregler wurde entsprechend
    erweitert. Optimierungen und Fehlerbehebungen wurden
    durchgeführt, um die neuen Funktionen zu unterstützen.

--- AnimationUebung/AnimationUebung/Form1.Designer.cs
+++ AnimationUebung/AnimationUebung/Form1.Designer.cs
@@ -41,19 +41,29 @@
             pbGruen = new PictureBox();
             timer2 = new System.Windows.Forms.Timer(components);
             RückwärtsButton = new Button();
+            pictureBox2 = new PictureBox();
+            pbGruen2 = new PictureBox();
+            pbGelb2 = new PictureBox();
+            pbRot2 = new PictureBox();
+            timer3 = new System.Windows.Forms.Timer(components);
+            timer4 = new System.Windows.Forms.Timer(components);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numGeschwindigkeitsregler).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbRot).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbGelb).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pbGruen).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbGruen2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbGelb2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbRot2).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(455, 318);
+            pictureBox1.Location = new Point(262, 192);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(179, 109);
+            pictureBox1.Size = new Size(90, 58);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -89,7 +99,7 @@
             // 
             StopButton.BackColor = Color.Red;
             StopButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            StopButton.Location = new Point(220, 73);
+            StopButton.Location = new Point(220, 66);
             StopButton.Name = "StopButton";
             StopButton.Size = new Size(132, 36);
             StopButton.TabIndex = 3;
@@ -101,7 +111,7 @@
             // 
             StartButton.BackColor = Color.Lime;
             StartButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            StartButton.Location = new Point(48, 73);
+            StartButton.Location = new Point(48, 66);
             StartButton.Name = "StartButton";
             StartButton.Size = new Size(132, 36);
             StartButton.TabIndex = 4;
@@ -112,9 +122,9 @@
             // pbRot
             // 
             pbRot.Image = (Image)resources.GetObject("pbRot.Image");
-            pbRot.Location = new Point(357, 133);
+            pbRot.Location = new Point(355, 118);
             pbRot.Name = "pbRot";
-            pbRot.Size = new Size(104, 179);
+            pbRot.Size = new Size(37, 68);
             pbRot.SizeMode = PictureBoxSizeMode.Zoom;
             pbRot.TabIndex = 5;
             pbRot.TabStop = false;
@@ -122,9 +132,9 @@
             // pbGelb
             // 
             pbGelb.Image = (Image)resources.GetObject("pbGelb.Image");
-            pbGelb.Location = new Point(357, 133);
+            pbGelb.Location = new Point(355, 118);
             pbGelb.Name = "pbGelb";
-            pbGelb.Size = new Size(104, 179);
+            pbGelb.Size = new Size(37, 68);
             pbGelb.SizeMode = PictureBoxSizeMode.Zoom;
             pbGelb.TabIndex = 6;
             pbGelb.TabStop = false;
@@ -132,9 +142,9 @@
             // pbGruen
             // 
             pbGruen.Image = (Image)resources.GetObject("pbGruen.Image");
-            pbGruen.Location = new Point(357, 133);
+            pbGruen.Location = new Point(355, 118);
             pbGruen.Name = "pbGruen";
-            pbGruen.Size = new Size(104, 179);
+            pbGruen.Size = new Size(37, 68);
             pbGruen.SizeMode = PictureBoxSizeMode.Zoom;
             pbGruen.TabIndex = 7;
             pbGruen.TabStop = false;
@@ -142,14 +152,14 @@
             // timer2
             // 
             timer2.Enabled = true;
-            timer2.Interval = 1000;
+            timer2.Interval = 800;
             timer2.Tick += timer2_Tick;
             // 
             // RückwärtsButton
             // 
             RückwärtsButton.BackColor = SystemColors.ActiveCaption;
             RückwärtsButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            RückwärtsButton.Location = new Point(394, 73);
+            RückwärtsButton.Location = new Point(394, 66);
             RückwärtsButton.Name = "RückwärtsButton";
             RückwärtsButton.Size = new Size(132, 36);
             RückwärtsButton.TabIndex = 8;
@@ -157,11 +167,61 @@
             RückwärtsButton.UseVisualStyleBackColor = false;
             RückwärtsButton.Click += RückwärtsButton_Click;
             // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(394, 307);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(90, 58);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // pbGruen2
+            // 
+            pbGruen2.Image = (Image)resources.GetObject("pbGruen2.Image");
+            pbGruen2.Location = new Point(355, 369);
+            pbGruen2.Name = "pbGruen2";
+            pbGruen2.Size = new Size(37, 68);
+            pbGruen2.SizeMode = PictureBoxSizeMode.Zoom;
+            pbGruen2.TabIndex = 12;
+            pbGruen2.TabStop = false;
+            // 
+            // pbGelb2
+            // 
+            pbGelb2.Image = (Image)resources.GetObject("pbGelb2.Image");
+            pbGelb2.Location = new Point(355, 369);
+            pbGelb2.Name = "pbGelb2";
+            pbGelb2.Size = new Size(37, 68);
+            pbGelb2.SizeMode = PictureBoxSizeMode.Zoom;
+            pbGelb2.TabIndex = 11;
+            pbGelb2.TabStop = false;
+            // 
+            // pbRot2
+            // 
+            pbRot2.Image = (Image)resources.GetObject("pbRot2.Image");
+            pbRot2.Location = new Point(355, 369);
+            pbRot2.Name = "pbRot2";
+            pbRot2.Size = new Size(37, 68);
+            pbRot2.SizeMode = PictureBoxSizeMode.Zoom;
+            pbRot2.TabIndex = 10;
+            pbRot2.TabStop = false;
+            // 
+            // timer3
+            // 
+            timer3.Enabled = true;
+            timer3.Interval = 800;
+            timer3.Tick += timer3_Tick;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pbGruen2);
+            Controls.Add(pbGelb2);
+            Controls.Add(pbRot2);
+            Controls.Add(pictureBox2);
             Controls.Add(RückwärtsButton);
             Controls.Add(pbGruen);
             Controls.Add(pbGelb);
@@ -178,6 +238,10 @@
             ((System.ComponentModel.ISupportInitialize)pbRot).EndInit();
             ((System.ComponentModel.ISupportInitialize)pbGelb).EndInit();
             ((System.ComponentModel.ISupportInitialize)pbGruen).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbGruen2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbGelb2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbRot2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -195,5 +259,11 @@
         private PictureBox pbGruen;
         private System.Windows.Forms.Timer timer2;
         private Button RückwärtsButton;
+        private PictureBox pictureBox2;
+        private PictureBox pbGruen2;
+        private PictureBox pbGelb2;
+        private PictureBox pbRot2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
     }
 }
--- AnimationUebung/AnimationUebung/Form1.cs
+++ AnimationUebung/AnimationUebung/Form1.cs
@@ -6,10 +6,12 @@ namespace AnimationUebung
 
         // 0 = rot, 1 = gelb, 2 = grün
         int phase = 0;
+        int phase2 = 0;
 
         int rueckwaerts = 0;
 
         PictureBox[] ampel = new PictureBox[3];
+        PictureBox[] ampel2 = new PictureBox[3];
 
         public Form1()
         {
@@ -18,13 +20,18 @@ namespace AnimationUebung
             ampel[0] = pbRot;
             ampel[1] = pbGelb;
             ampel[2] = pbGruen;
+
+            ampel2[0] = pbRot2;
+            ampel2[1] = pbGelb2;
+            ampel2[2] = pbGruen2;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            // Fahrzeug 1 bewegen
             if (rueckwaerts == 0)
             {
-                if (!(phase == 0 && pictureBox1.Location.X >= 300 && pictureBox1.Location.X <= 350))
+                if (!(phase == 0 && pictureBox1.Location.X >= 390 && pictureBox1.Location.X <= 410))
                 {
                     pictureBox1.Location = new Point(pictureBox1.Location.X - geschwindigkeit, pictureBox1.Location.Y);
 
@@ -36,13 +43,39 @@ namespace AnimationUebung
             }
             else
             {
-                if (!(phase == 0 && pictureBox1.Location.X >= 130 && pictureBox1.Location.X<= 175))
+                if (!(phase == 0 && pictureBox1.Location.X >= 260 && pictureBox1.Location.X <= 280))
                 {
                     pictureBox1.Location = new Point(pictureBox1.Location.X + geschwindigkeit, pictureBox1.Location.Y);
 
                     if (pictureBox1.Location.X > this.Width)
                     {
                         pictureBox1.Location = new Point(0, pictureBox1.Location.Y);
+                    }
+                }
+            }
+
+            // Fahrzeug 2 bewegen – dieselbe Logik, andere Position prüfen
+            if (rueckwaerts == 0)
+            {
+                if (!(phase2 == 0 && pictureBox2.Location.X >= 390 && pictureBox2.Location.X <= 410))
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X - geschwindigkeit, pictureBox2.Location.Y);
+
+                    if (pictureBox2.Location.X < -pictureBox2.Width)
+                    {
+                        pictureBox2.Location = new Point(this.Width, pictureBox2.Location.Y);
+                    }
+                }
+            }
+            else
+            {
+                if (!(phase2 == 0 && pictureBox2.Location.X >= 260 && pictureBox2.Location.X <= 280))
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X + geschwindigkeit, pictureBox2.Location.Y);
+
+                    if (pictureBox2.Location.X > this.Width)
+                    {
+                        pictureBox2.Location = new Point(0, pictureBox2.Location.Y);
                     }
                 }
             }
@@ -64,10 +97,11 @@ namespace AnimationUebung
         {
             numGeschwindigkeitsregler.Value = 0;
         }
+
         private void RückwärtsButton_Click(object sender, EventArgs e)
         {
-           // numGeschwindigkeitsregler.Value = -1;
-            rueckwaerts =1;
+            numGeschwindigkeitsregler.Value = 1;
+            rueckwaerts = 1;
         }
 
         private void timer2_Tick(object sender, EventArgs e)
@@ -113,6 +147,51 @@ namespace AnimationUebung
             }
             // Phase erhöhen
             phase = (phase + 1) % 7;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            // Alle Bilder zuerst unsichtbar machen
+            pbRot2.Visible = false;
+            pbGelb2.Visible = false;
+            pbGruen2.Visible = false;
+
+            // Bild je nach Phase sichbar machen
+            switch (phase2)
+            {
+                // Phase 0 = rot
+                case 0:
+                    pbRot2.Visible = true;
+                    break;
+                // Phase 1 = Gelb/Rot
+                case 1:
+                    pbRot2.Visible = true;
+                    pbGelb2.Visible = true;
+                    break;
+                // Phase 2 = Gelb
+                case 2:
+                    pbGelb2.Visible = true;
+                    break;
+                // Phase 3 = grün
+                case 3:
+                    pbGruen2.Visible = true;
+                    break;
+                // Phase 4 = GRün/Gelb
+                case 4:
+                    pbGruen2.Visible = true;
+                    pbGelb2.Visible = true;
+                    break;
+                // Phase 5 = Gelb
+                case 5:
+                    pbGelb2.Visible = true;
+                    break;
+                // Phase 6 = Rot
+                case 6:
+                    pbRot2.Visible = true;
+                    break;
+            }
+            // Phase erhöhen
+            phase2 = (phase2 + 1) % 7;
         }
     }
 }
