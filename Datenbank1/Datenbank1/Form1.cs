417bd3c997e73473864c6808fe919f7c1b7491ba
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-05-26 14:00:58 +0200

    Warengruppen einlesen und anzeigen implementiert
    Eine neue Methode `wgAnzeigen()` wurde hinzugefügt, die die
    `listBox1` leert und die Bezeichnungen der Warengruppen
    (`WgBezeichnung`) aus der Liste `liWaGr` einfügt.
    
    Im Event-Handler `btnEinlesen_Click` wird die Liste `liWaGr`
    vor der Datenbankabfrage geleert, um alte Daten zu vermeiden.
    Eine SQL-Abfrage (`SELECT * FROM warengruppe`) liest alle
    Warengruppen aus der Tabelle `warengruppe`. Die Ergebnisse
    werden mit einem `MySqlDataReader` ausgelesen und als
    `Warengruppe`-Objekte in `liWaGr` gespeichert.
    
    Abschließend wird `wgAnzeigen()` aufgerufen, um die
    eingelesenen Warengruppen in der `listBox1` darzustellen.

--- Datenbank1/Datenbank1/Form1.cs
+++ Datenbank1/Datenbank1/Form1.cs
@@ -17,12 +17,34 @@ namespace Datenbank1
             conn = new MySqlConnection(connstr);
         }
 
+        private void wgAnzeigen()
+        {
+
+            listBox1.Items.Clear();
+            foreach (Warengruppe wg in liWaGr)
+            {
+                listBox1.Items.Add(wg.WgBezeichnung);
+            }
+        }
         private void btnEinlesen_Click(object sender, EventArgs e)
         {
             try
             {
+                liWaGr.Clear();
                 MySqlCommand com = conn.CreateCommand();
                 conn.Open();
+                com.CommandText = "SELECT * FROM warengruppe";
+                MySqlDataReader reader = com.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    liWaGr.Add(new Warengruppe(
+                        reader.GetInt32(0), //  weitere Variante("wgNr") 
+                        reader.GetString(1) // weitere Variante("wgBezeichnung")
+                    ));
+                }
+                reader.Close();
+                wgAnzeigen(); // Optional: Löschen der Liste nach dem Anzeigen, falls nicht mehr benötigt wird
             }
             catch (Exception ex)
             {
