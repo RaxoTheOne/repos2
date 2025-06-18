8e112502ca5d9b306af94315a59e07957f2aaa8e
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-06-18 20:53:13 +0200

    Initiales WPF-Projekt mit UI und Logik angelegt
    Projekt- und Lösungsdateien für "WpfApp1" hinzugefügt. Zentrales WPF-UI mit drei TextBoxen, Button und Label in MainWindow.xaml erstellt. In MainWindow.xaml.cs Logik implementiert, um TextBox-Inhalte beim Button-Klick im Label anzuzeigen. In Benutzer.cs unnötiges using entfernt und relevante using-Direktiven ergänzt. AssemblyInfo.cs mit ThemeInfo-Attributen hinzugefügt.

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btninsert_Click(object sender, RoutedEventArgs e)
+        {
+            lbinfo.Content = Vorname.Text + nachname.Text + telefon.Text;
+        }
+    }
+}
